9c74fac580a6709807787c1a6c1876bb0812d3eb
jerome <hue.jerome@yahoo.fr>
2021-04-28 08:44:21 +0200

    key words identification

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import sys
+from keybert import KeyBERT
 
 def main():
     if (len(sys.argv) != 2):
@@ -9,7 +10,15 @@ def main():
     with open(filename, 'r') as file:
         text = file.read()
         print(text)
-        # Call a module?
+    # Call a module?
+    
+    text = "The quick brown fox jumps over the lazy dog."
+
+    kw_extractor = KeyBERT('distilbert-base-nli-mean-tokens')
+    for j in range(len(array_text)):
+        keywords = kw_extractor.extract_keywords(text, stop_words='english')
+    print("Keywords of article", keywords)
+    
 
 # Guard preventing this script to be run more
 # than once when being imported from other scripts
