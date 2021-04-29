74308ddf1f0d0edeb1c492188848b0084a8a3fc8
Charly Poirier <pro.poiriercharly@gmail.com>
2021-04-29 12:04:30 +0200

    Adjective replacements for boolean questions

--- questions/boolean.py
+++ questions/boolean.py
@@ -1,7 +1,7 @@
-from questions.Question import Question
+import random
 import spacy
-
-nlp = spacy.load("en_core_web_sm")
+from questions.Question import Question
+from spacy_wordnet.wordnet_annotator import WordnetAnnotator 
 
 """
 Boolean questions
@@ -11,16 +11,37 @@ Boolean questions
 4) Extract subject-verb-complement
 """
 
+# Global variables
+nlp = spacy.load('en_core_web_sm')
+nlp.add_pipe(WordnetAnnotator(nlp.lang))
+
+def change_adjective(sentence):
+    document = nlp(sentence)
+    for token in document:
+        if token.pos_ in ["ADJ"]:
+            synonyms = random.choice(token._.wordnet.synsets())
+            synonym = random.choice(synonyms.lemma_names())
+            sentence = sentence.replace(token.text, synonym)
+            print("(" + token.text + "=>" + synonym + ") ", end="")
+        else:
+            print(token.text + " ", end="")
+    print()
+    return sentence
+
 def generate(text):
 
     # Split into sentences
+    nlp = spacy.load("en_core_web_sm")
     document = nlp(text)
     sentences = document.sents
 
     # Generate questions
     questions = set()
     for sentence in sentences:
-        question = Question(sentence.text, "True", ["False"])
+        # Replace adjectives with their synonyms
+        new_sentence = change_adjective(sentence.text)
+        # Create the question
+        question = Question(new_sentence, "True", ["False"])
         questions.add(question)
 
     return questions
