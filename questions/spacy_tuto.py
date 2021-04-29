9bfed21d01c2f01ae0176eb629d62084a5499b70
jerome <hue.jerome@yahoo.fr>
2021-04-29 11:44:06 +0200

    list of entities

--- questions/spacy_tuto.py
+++ questions/spacy_tuto.py
@@ -3,9 +3,10 @@ import spacy
 
 nlp = spacy.load('en_core_web_trf')
 labels = nlp.get_pipe("tagger").labels
+ent  = nlp.get_pipe("ner").labels
 
 # tous les labels
-print(labels)
+print(ent)
 
 # et les explications qui vont avec
 for tag in labels:
