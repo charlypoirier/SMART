e5e4db9f7b3927e51c970605691992df50d4207e
Quentin Regaud <quentin.regaud31@gmail.com>
2021-04-30 10:30:58 +0200

    gérer les keywords

--- questions/gaps.py
+++ questions/gaps.py
@@ -45,32 +45,52 @@ def main():
     wnl = WordNetLemmatizer()
 
     origin_nbwords = nbwords
+    sg_word_to_add = []
     end = False
     while not end:
         n_keywords = keywords(text, nbwords)
         nb_diff_words = 0
         for word in n_keywords:
-            #print(word)
+            print(word)
             word_sg = wnl.lemmatize(word) #Mets au singulier
             if word != word_sg:
                 if word_sg not in n_keywords:
                     nb_diff_words += 1
+                    sg_word_to_add.append(word_sg)
+                else:
+                    print("Plural word exists whithout singular")
             else:
                 nb_diff_words += 1
-
-        print("nb-diff_words", nb_diff_words, nbwords)
+        n_keywords = n_keywords + sg_word_to_add
+        
+        print("nb-diff_words", nb_diff_words, nbwords, origin_nbwords)
         if (origin_nbwords - nb_diff_words > 0):
             nbwords += (origin_nbwords - nb_diff_words)
         else:
             end = True
         print("\nKeywords of article", n_keywords,)
 
-    for word in n_keywords:
-        text = text.replace(word, '___')
-        text = text.replace(word.capitalize(), '___')
-        text = text.replace('___s', '___')
+    
+    through_text(n_keywords, text)
+
+    """text = text.replace(word, '___')
+    text = text.replace(word.capitalize(), '___')
+    text = text.replace('___s', '___')"""
 
     print(text)
+
+def through_text(n_keywords, text):
+    text_words_list = text.split(" ")
+    i = 0
+    tab_answers =[]
+    new_text_words_list = []
+    for word in text_words_list:
+        if word is n_keywords:
+            tab_answers.append(word)
+            new_text_words_list = text_words_list
+            new_text_words_list[i] = "[MASK]"
+        i+=1    
+
 
 
 main()
