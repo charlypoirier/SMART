1d0e15e310d661161d76f32304d5220cc260393f
Charly Poirier <pro.poiriercharly@gmail.com>
2021-04-30 10:38:49 +0200

    Better formatting for to_aiken()

--- classes/question.py
+++ classes/question.py
@@ -1,14 +1,13 @@
 class Question:
     
-    # Constructor
     def __init__(self, stem, options, answer):
         self.stem = stem
         self.options = options
         self.answer = answer
 
     def to_aiken(self):
-        aiken = self.stem + '\n'
+        aiken = self.stem + "\n"
         for i in range(len(self.options)):
-            aiken += chr(ord('A')+i) + '. ' + self.options[i] + '\n'
-        aiken += 'ANSWER: '+chr(ord('A') + self.answer) + '\n\n'
+            aiken += f"{chr(ord('A')+i)}. {self.options[i]}\n"
+        aiken += f"ANSWER: {chr(ord('A')+self.answer)}\n\n"
         return aiken
